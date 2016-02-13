8cdf954bf38b76cdd4fcd5ed097ac5df3b255423
Hiromichi Matsushima <hylom@Hiromichi-no-iMac.local>
2016-02-14 05:19:20 +0900

    implementing tlexi

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 // tlexi.js
 
-var tlexi = require('lib/tlexi');
+var tlexi = require('./lib/tlexi');
 var commander = require('commander');
+var fs = require('fs');
 
-commander.option('-p, --parser <file>', 'parser file')
-  .parse(process.argv);
+commander.version('0.0.1')
+  .option('-p, --parser <file>', 'parser file')
+  .arguments('<filename>')
+  .action( function (filename) {
+    commander.filename = filename;
+  })
+  .parse(process.argv)
 
 if (!commander.parser) {
-  console.log('you must give -p option');
+  commander.outputHelp();
+  console.log('error: you must give -p option');
   process.exit(1);
 }
 
+if (!commander.filename) {
+  commander.outputHelp();
+  process.exit(1);
+}
 
+function main() {
+  var t = tlexi.load(commander.parser);
+  var text = fs.readFileSync(commander.filename, 'utf8');
+  t.parseText(text);
+}
+
+main();
